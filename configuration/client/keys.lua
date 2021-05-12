1e4c6ebb18499669a988e8bb6d37c528a3718106
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-05-11 21:06:46 -0500

    reorganize lua code

--- configuration/client/keys.lua
+++ configuration/client/keys.lua
@@ -2,26 +2,35 @@ local awful = require('awful')
 require('awful.autofocus')
 local modkey = require('configuration.keys.mod').modKey
 local altkey = require('configuration.keys.mod').altKey
+local pageUp = '#112'
+local pageDown = '#117'
 
 local clientKeys =
-  awful.util.table.join(
-  awful.key(
-    {modkey},
-    'f',
-    function(c)
-      c.fullscreen = not c.fullscreen
-      c:raise()
-    end,
-    {description = 'toggle fullscreen', group = 'client'}
-  ),
-  awful.key(
-    {modkey},
-    'q',
-    function(c)
-      c:kill()
-    end,
-    {description = 'close', group = 'client'}
-  )
-)
+    awful.util.table.join(
+        awful.key({modkey}, 'F11',
+            function(c)
+                c.fullscreen = not c.fullscreen
+                c:raise()
+            end,
+            {description = 'Toggle Fullscreen', group = 'Windows'}
+        ),
+        awful.key({altkey}, 'F4',
+            function(c)
+                c:kill()
+            end,
+            {description = 'Close Window', group = 'Windows'}
+        ),
+        awful.key({modkey}, pageDown,
+            function(c)
+                c.minimized = true
+            end ,
+            {description = "Minimize", group = "Windows"}),
+        awful.key({modkey}, pageUp,
+            function(c)
+                c.maximized = not c.maximized
+                c:raise()
+            end ,
+            {description = "Maximize", group = "Windows"})
+    )
 
 return clientKeys
