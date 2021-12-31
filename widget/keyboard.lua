e4326b78072869f6535c92201c4e5475f5b3cb1c
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-12-31 09:16:18 -0500

    keyboard widget to show layout with no variant

--- widget/keyboard.lua
+++ widget/keyboard.lua
@@ -3,16 +3,20 @@ local wibox = require('wibox')
 local awful = require('awful')
 
 local function Keyboard(s, color)
-  return {
-    {
-      font_icons.make_faicon(font_icons.keyboard, color.hue_500),
-      {{widget = awful.widget.keyboardlayout}, fg = color.hue_100, widget = wibox.container.background},
-      layout = wibox.layout.fixed.horizontal
-    },
-    left = 5,
-    right = 5,
-    widget = wibox.container.margin
-  }
+    return {
+        {
+            font_icons.make_faicon(font_icons.keyboard, color.hue_500),
+            {
+                awful.widget.watch([[bash -c "localectl status | grep Layout | cut -d ':' -f2- && echo ''"]], 15),
+                fg = color.hue_100, 
+                widget = wibox.container.background
+            },
+            layout = wibox.layout.fixed.horizontal
+        },
+        left = 5,
+        right = 5,
+        widget = wibox.container.margin
+    }
 end
 
 return Keyboard
