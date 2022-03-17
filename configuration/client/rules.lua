4ace710914f0580ba57191b826e2b173e1b5eee4
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-03-17 17:00:52 -0400

    zoom settings

--- configuration/client/rules.lua
+++ configuration/client/rules.lua
@@ -6,7 +6,7 @@ local client_buttons = require('configuration.client.buttons')
 -- Rules
 awful.rules.rules = {
     -- All clients will match this rule.
-{
+    {
         rule = {},
         properties = {
             focus = awful.client.focus.filter,
@@ -25,12 +25,11 @@ awful.rules.rules = {
             maximized_vertical = false
         }
     },
-{
+    {
         rule_any = {name = {'QuakeTerminal'}},
         properties = {skip_decoration = true}
     },
-{
-
+    {
         rule = { name = 'Picture-in-Picture'},
         properties = {
             floating = true,
@@ -42,8 +41,22 @@ awful.rules.rules = {
             skip_decoration = true
         }
     },
-{
-
+    {
+        rule = { class = 'zoom', name = 'Settings'},
+        properties = {
+            placement = awful.placement.centered,
+            ontop = true,
+            floating = true,
+            drawBackdrop = true,
+            shape = function()
+                return function(cr, w, h)
+                    gears.shape.rounded_rect(cr, w, h, 8)
+                end
+            end,
+            skip_decoration = true
+        }
+    },
+    {
         rule = { class = 'jetbrains-.*', name = 'win0'},
         properties = {
             placement = awful.placement.centered,
@@ -59,7 +72,7 @@ awful.rules.rules = {
         }
     },
     -- Titlebars
-{
+    {
         rule_any = {type = {'dialog'}, class = {'Wicd-client.py', 'calendar.google.com'}, role = {'Popup'}},
         properties = {
             placement = awful.placement.centered,
