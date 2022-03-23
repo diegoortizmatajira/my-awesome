470109f791bef968d26bd65a07de8a9f95176fbd
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-03-23 17:23:24 -0400

    Zoom share screen window floating

--- configuration/client/rules.lua
+++ configuration/client/rules.lua
@@ -5,86 +5,113 @@ local client_buttons = require('configuration.client.buttons')
 
 -- Rules
 awful.rules.rules = {
-    -- All clients will match this rule.
-    {
-        rule = {},
-        properties = {
-            focus = awful.client.focus.filter,
-            raise = true,
-            keys = client_keys,
-            buttons = client_buttons,
-            screen = awful.screen.preferred,
-            placement = awful.placement.no_offscreen,
-            floating = false,
-            maximized = false,
-            above = false,
-            below = false,
-            ontop = false,
-            sticky = false,
-            maximized_horizontal = false,
-            maximized_vertical = false
-        }
-    },
-    {
-        rule_any = {name = {'QuakeTerminal'}},
-        properties = {skip_decoration = true}
-    },
-    {
-        rule = { name = 'Picture-in-Picture'},
-        properties = {
-            floating = true,
-            shape = function()
-                return function(cr, w, h)
-                    gears.shape.rounded_rect(cr, w, h, 8)
-                end
-            end,
-            skip_decoration = true
-        }
-    },
-    {
-        rule = { class = 'zoom', name = 'Settings'},
-        properties = {
-            placement = awful.placement.centered,
-            ontop = true,
-            floating = true,
-            drawBackdrop = true,
-            shape = function()
-                return function(cr, w, h)
-                    gears.shape.rounded_rect(cr, w, h, 8)
-                end
-            end,
-            skip_decoration = true
-        }
-    },
-    {
-        rule = { class = 'jetbrains-.*', name = 'win0'},
-        properties = {
-            placement = awful.placement.centered,
-            ontop = true,
-            floating = true,
-            drawBackdrop = true,
-            shape = function()
-                return function(cr, w, h)
-                    gears.shape.rounded_rect(cr, w, h, 8)
-                end
-            end,
-            skip_decoration = true
-        }
-    },
-    -- Titlebars
-    {
-        rule_any = {type = {'dialog'}, class = {'Wicd-client.py', 'calendar.google.com'}, role = {'Popup'}},
-        properties = {
-            placement = awful.placement.centered,
-            ontop = true,
-            floating = true,
-            drawBackdrop = true,
-            shape = function()
-                return function(cr, w, h)
-                    gears.shape.rounded_rect(cr, w, h, 8)
-                end
-            end,
-            skip_decoration = true
-        }
-    },
+  -- All clients will match this rule.
+  {
+    rule = {},
+    properties = {
+      focus = awful.client.focus.filter,
+      raise = true,
+      keys = client_keys,
+      buttons = client_buttons,
+      screen = awful.screen.preferred,
+      placement = awful.placement.no_offscreen,
+      floating = false,
+      maximized = false,
+      above = false,
+      below = false,
+      ontop = false,
+      sticky = false,
+      maximized_horizontal = false,
+      maximized_vertical = false
+    }
+  },
+  {rule_any = {name = {'QuakeTerminal'}}, properties = {skip_decoration = true}},
+  {
+    rule = {name = 'Picture-in-Picture'},
+    properties = {
+      floating = true,
+      shape = function()
+        return function(cr, w, h)
+          gears.shape.rounded_rect(cr, w, h, 8)
+        end
+      end,
+      skip_decoration = true
+    }
+  },
+  {
+    rule = {class = 'zoom', name = 'Settings'},
+    properties = {
+      placement = awful.placement.centered,
+      ontop = true,
+      floating = true,
+      drawBackdrop = true,
+      shape = function()
+        return function(cr, w, h)
+          gears.shape.rounded_rect(cr, w, h, 8)
+        end
+      end,
+      skip_decoration = true
+    }
+  },
+  {
+    rule = {class = 'zoom', name = 'Select a window or an application that you want to share'},
+    properties = {
+      placement = awful.placement.centered,
+      ontop = true,
+      floating = true,
+      drawBackdrop = true,
+      shape = function()
+        return function(cr, w, h)
+          gears.shape.rounded_rect(cr, w, h, 8)
+        end
+      end,
+      skip_decoration = true
+    }
+  },
+  {
+    rule = {class = 'jetbrains-.*', name = 'Welcome to JetBrains Rider'},
+    properties = {
+      placement = awful.placement.centered,
+      ontop = true,
+      floating = true,
+      drawBackdrop = true,
+      shape = function()
+        return function(cr, w, h)
+          gears.shape.rounded_rect(cr, w, h, 8)
+        end
+      end,
+      skip_decoration = true
+    }
+  },
+  {
+    rule = {class = 'jetbrains-.*', name = 'win0'},
+    properties = {
+      placement = awful.placement.centered,
+      ontop = true,
+      floating = true,
+      drawBackdrop = true,
+      shape = function()
+        return function(cr, w, h)
+          gears.shape.rounded_rect(cr, w, h, 8)
+        end
+      end,
+      skip_decoration = true
+    }
+  },
+  -- Titlebars
+  {
+    rule_any = {type = {'dialog'}, class = {'Wicd-client.py', 'calendar.google.com'}, role = {'Popup'}},
+    properties = {
+      placement = awful.placement.centered,
+      ontop = true,
+      floating = true,
+      drawBackdrop = true,
+      shape = function()
+        return function(cr, w, h)
+          gears.shape.rounded_rect(cr, w, h, 8)
+        end
+      end,
+      skip_decoration = true
+    }
+  }
 }
