3d9ec4a3eb528d53c16e934cb6d59e2d78240489
Diego Ortiz <diego.ortiz@bairesdev.com>
2022-04-19 16:54:14 -0400

    Fix warnings using luacheck inline comments

--- widget/network.lua
+++ widget/network.lua
@@ -2,7 +2,7 @@ local font_icons = require('widget.font-icons')
 local wibox = require('wibox')
 local clickable_container = require('widget.material.clickable-container')
 
-local function Network(s, color)
+local function Network(_, color)
     local custom_widget = {
         {font_icons.make_faicon(font_icons.network, color.hue_500), layout = wibox.layout.fixed.horizontal},
         left = 5,
